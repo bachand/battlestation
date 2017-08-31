81aadb25acbc9b0dfd634bbafc0845b76bda1401
Michael Bachand <bachand.michael@gmail.com>
2017-08-31 00:00:25 -0700

    Start to move to a library world.

---
+++ lib/battlestation/cli.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Battlestation
+
+  # Responsible of handling all the command line interface logic.
+  class CLI
+
+    def initialize
+      puts "hello world"
+    end
+  end
+end
