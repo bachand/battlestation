2907371ca7f8192665c386877e60308359965498
Michael Bachand <bachand.michael@gmail.com>
2018-03-31 00:37:26 -0700

    Install rbenv and start breaking apart bash

--- lib/battlestation/cli.rb
+++ lib/battlestation/cli.rb
@@ -13,12 +13,48 @@ module Battlestation
     # @param args [Array<String>] command line arguments
     # @return [Integer] UNIX exit code
     def run(args = ARGV)
-      current_pathname = Pathname.new(__FILE__)
-      setup_path = File.join current_pathname.dirname, '../../bin/setup'
+      current_dirname = Pathname.new(__FILE__).dirname
 
-      system 'bash', '-c', setup_path
+      run_legacy_setup_script(current_dirname)
+
+      install_packages(current_dirname)
 
       return $?.exitstatus
+    end
+
+    private
+
+    def run_legacy_setup_script(current_dirname)
+      setup_path = File.join current_dirname, '../../bin/setup'
+      system 'bash', '-c', setup_path
+    end
+
+    def install_packages(current_dirname)
+      # Slowly bringing code into ruby from the monolithic setup script.
+      system 'bash', '-c', %{
+#######################################
+# Installs the specified Homebrew package if it isn't installed. If it is, tries to upgrade the
+# package.
+#
+# Arguments:
+#   Name of package
+#######################################
+install_or_upgrade_package() {
+  if ! brew list "$1" >/dev/null 2>&1; then
+    echo_info "Installing $1 via Homebrew"
+    brew install "$1"
+  else
+    # TODO: find a way to only update if needed to prevent errors in console.
+    brew upgrade "$1"
+  fi
+}
+
+packages=( git ag fzf rbenv )
+for package in "${packages[@]}"
+do
+  install_or_upgrade_package "$package"
+done
+      }
     end
   end
 end
