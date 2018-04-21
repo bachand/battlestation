ff260b3b15855016139d73cf32cb725459bd6585
Michael Bachand <bachand.michael@gmail.com>
2018-04-20 22:35:39 -0700

    Modularize utils

--- lib/battlestation/utils.rb
+++ lib/battlestation/utils.rb
@@ -4,11 +4,14 @@ module Battlestation
 
   module Utils
 
-    def self.exit_on_error(message = "")
+    def self.put_error(message)
       if ! message.empty? && ! message.nil?
         STDERR.puts "#{message}\n"
       end
+    end
 
+    def self.exit_on_error(message = "")
+      put_error(message)
       exit 1
     end
   end
