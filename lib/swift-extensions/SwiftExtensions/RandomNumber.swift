05467a44e394131c518c18892dcfb7d5b4656750
Michael Bachand <bachand.michael@gmail.com>
2018-05-12 02:48:58 -0700

    Add first pass at random number

---
+++ lib/swift-extensions/SwiftExtensions/RandomNumber.swift
@@ -0,0 +1,41 @@
+//
+//  RandomNumber.swift
+//  SwiftExtensions
+//
+//  Created by Michael Bachand on 5/12/18.
+//
+
+import Darwin
+
+// MARK: - RandomNumber
+
+public struct RandomNumber {
+  typealias ValueType = Int
+  
+  /// - param allowedValues: Count must be fit in a UInt32.
+  init(_ allowedValues: CountableClosedRange<ValueType> = 0...9) throws {
+    guard allowedValues.lowerBound < 0 else { throw RandomNumberError.notImplemented }
+    guard UInt32(exactly: allowedValues.count) != nil else { throw RandomNumberError.tooLarge }
+    self.allowedValues = allowedValues
+  }
+  
+  // MARK: Private
+  
+  private let allowedValues: CountableClosedRange<ValueType>
+  
+  lazy var value: ValueType = {
+    // This downcase is OK since we assert in the initalizer.
+    let arc4random_upper_bound = UInt32(allowedValues.upperBound)
+    return Int(arc4random_uniform(arc4random_upper_bound))
+  }()
+}
+
+// MARK: - RandomNumberError
+
+public enum RandomNumberError: Error {
+  /// The count of `allowedValues` must fit in a UInt32 due to limitations in Apple's `arc4random` library.
+  case tooLarge
+  /// This fucntionality is intended to exist but hasn't yet been implemented.
+  case notImplemented
+}
+
