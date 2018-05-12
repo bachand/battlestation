aab5c3abc4491e186548e2e1356c657476e14a4a
Michael Bachand <bachand.michael@gmail.com>
2018-05-12 11:59:07 -0700

    Add in a test

---
+++ lib/swift-extensions/SwiftExtensionsTests/RandomNumberTests.swift
@@ -0,0 +1,29 @@
+//
+//  RandomNumberTests.swift
+//  SwiftExtensionsTests
+//
+//  Created by Michael Bachand on 5/12/18.
+//
+
+import XCTest
+@testable import SwiftExtensions
+
+final class RandomNumberTests: XCTestCase {
+
+  func testThatAllAllowedValuesAreUtilized() {
+    let allowedValues = 0...100
+
+    // Let's assume that if we run this `allowedValues.upperBound` * 100 times that it would be
+    // _highly improbable_ not to see each value. As such, even though this test isn't guaranteed
+    // to succeed, the false negative rate should be acceptably low.
+    var seenValueCounts = Array<RandomNumber.Value>.init(repeating: 0, count: allowedValues.count)
+    for _ in 0...(allowedValues.upperBound * 100) {
+      var randomNumber = try! RandomNumber(allowedValues)
+      seenValueCounts[randomNumber.value] += 1
+    }
+
+    for value in allowedValues {
+      XCTAssertTrue(seenValueCounts[value] > 0)
+    }
+  }
+}
