6adc8f7a9063228d3b86374fa1f3626a596b8f64
Michael Bachand <bachand.michael@gmail.com>
2018-06-19 19:11:41 -0700

    Add alias for S3

--- config/dotfiles/aliases.bash
+++ config/dotfiles/aliases.bash
@@ -10,5 +10,7 @@ alias ls='ls -alG'
 alias sn='code'
 alias cdr="cd $HOME/repos"
 
+alias s3='aws sl'
+
 # Apple says that anything requiring a developer to delete `DerivedData` is a serious bug in Xcode.
 alias rmdd="echo I\'m sorry `id -un`, I\'m afraid I can\'t do that"
