f583ca0adfb6641dc9758e3fbcc51d283e61818a
Michael Bachand <bachand.michael@gmail.com>
2018-06-19 19:06:32 -0700

    Move aliases to their own file

---
+++ config/dotfiles/aliases.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+alias g='git'
+alias be='bundle exec'
+alias ber='bundle exec rake'
+
+alias ytdl='youtube-dl'
+
+alias ls='ls -alG'
+alias sn='code'
+alias cdr="cd $HOME/repos"
+
+# Apple says that anything requiring a developer to delete `DerivedData` is a serious bug in Xcode.
+alias rmdd="echo I\'m sorry `id -un`, I\'m afraid I can\'t do that"
