cff12c6699191e0b72bff9c71c99a24771be3edc
Michael Bachand <michael.bachand@airbnb.com>
2021-11-27 00:58:28 -0800

    Use rbi instead of git rbm

--- config/dotfiles/aliases.zsh
+++ config/dotfiles/aliases.zsh
@@ -17,3 +17,7 @@ alias s3='aws s3'
 
 # Apple says that anything requiring a developer to delete `DerivedData` is a serious bug in Xcode.
 alias rmdd="echo I\'m sorry `id -un`, I\'m afraid I can\'t do that"
+
+# Rebase the current branch over where it branched from master.
+# TODO: Change to main.
+alias rbi="git rebase -i \$(git merge-base HEAD origin/master)"
