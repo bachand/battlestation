0955821b337271be3d8e1bedde03b5aff45a1ff9
Michael Bachand <bachand.michael@gmail.com>
2024-07-02 13:52:18 -0700

    remove AWS CLI aliases

--- config/dotfiles/aliases.zsh
+++ config/dotfiles/aliases.zsh
@@ -12,9 +12,6 @@ alias cdr="cd $HOME/repos"
 alias sn='code'
 alias v='vim'
 
-alias cloudformation='aws cloudformation'
-alias s3='aws s3'
-
 # Apple says that anything requiring a developer to delete `DerivedData` is a serious bug in Xcode.
 alias rmdd="echo I\'m sorry `id -un`, I\'m afraid I can\'t do that"
 
