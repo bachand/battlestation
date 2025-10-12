e15287e3f9871013b10637450037485cf8002e0f
Michael Bachand <bachand.michael@gmail.com>
2025-10-11 17:09:28 -0700

    add test

---
+++ spec/battlestation/cli_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require_relative '../../lib/battlestation/cli'
+
+describe Battlestation::CLI do
+
+  describe '#run' do
+    it 'runs each setup step and returns shell exit status' do
+      expect(subject).to receive(:install_terminal_theme).with(kind_of(Pathname))
+      expect(subject).to receive(:configure_xcode)
+      expect(subject).to receive(:run_legacy_setup_script).with(kind_of(Pathname))
+      expect(subject).to receive(:install_python)
+      expect(subject).to receive(:install_aws_cli)
+      expect(subject).to receive(:update_homebrew)
+      expect(subject).to receive(:install_packages)
+      expect(subject).to receive(:configure_fzf)
+      expect(subject).to receive(:install_ruby).with('2.7.6')
+      expect(subject).to receive(:set_ruby_version).with('2.7.6')
+      expect(subject).to receive(:install_gems).with(kind_of(Pathname)) { system('true') }
+
+      allow(Output).to receive(:put_success)
+      allow(Output).to receive(:put_info)
+
+      expect(subject.run).to eq(0)
+    end
+  end
+end
